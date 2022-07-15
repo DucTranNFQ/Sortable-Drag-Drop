1d420015dae4f23ec868123c2ff74c1ddc734ae1
Duc Tran <duc.tran@nfq.asia>
2022-07-15 09:33:24 +0700

    commit 2

--- script.js
+++ script.js
@@ -12,8 +12,33 @@ draggables.forEach((draggable) => {
 })
 
 containers.forEach((container) => {
-  container.addEventListener("dragover", () => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
     const draggable = document.querySelector(".dragging")
-    container.appendChild(draggable)
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
   })
 })
+
+const getDragAfterElement = (container, y) => {
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ]
+  return draggableElements.reduce(
+    (closest, child) => {
+      console.log(closest)
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, elemment: child }
+      } else {
+        return closest
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).elemment
+}
